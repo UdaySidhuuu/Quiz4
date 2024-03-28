94a9ee2b470c375f1bb6c089c192fe513882c3b6
Uday Sidhu <usidhu5@my.centennialcollege.ca>
2024-03-28 14:22:01 -0400

    Initial Commit

---
+++ Quiz4Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Quiz4Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
